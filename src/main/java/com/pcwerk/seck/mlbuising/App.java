6a9b1685df3773546bf8e3135a19f55a8e863d47
Mark Buising <mlbuising@gmail.com>
2012-09-24 10:57:16 -0700

    Updated output with correct information

--- src/main/java/com/pcwerk/seck/mlbuising/App.java
+++ src/main/java/com/pcwerk/seck/mlbuising/App.java
@@ -8,7 +8,7 @@ package com.pcwerk.seck.mlbuising;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World from Mark Buising!");
 	}
 
 }
