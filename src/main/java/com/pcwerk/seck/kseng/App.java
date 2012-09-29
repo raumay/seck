c3b455cb221425f19dfa49bda4a6ff38a3b5ed68
Kevin Seng <zyrmpg@gmail.com>
2012-09-29 14:21:04 -0700

    Hello World

--- src/main/java/com/pcwerk/seck/kseng/App.java
+++ src/main/java/com/pcwerk/seck/kseng/App.java
@@ -1,9 +1,9 @@
-+package com.pcwerk.seck.cs454nm;
-+
-+public class App 
-+{
-+	public static void main(String[] args) 
-+	{
-+		System.out.println("Hello World from Kevin Seng!");
-+	}
-+}
+package com.pcwerk.seck.kseng;
+
+public class App 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World from Kevin Seng!");
+	}
+}
